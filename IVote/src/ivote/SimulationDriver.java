6e597e7b97d62d6a4fb9a19bb02c05fb4c92f7b9
aaronmson <aaronmsun@yahoo.com>
2015-08-12 12:40:42 +0530

    Classes created

---
+++ IVote/src/ivote/SimulationDriver.java
@@ -0,0 +1,12 @@
+package ivote;
+
+public class SimulationDriver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
