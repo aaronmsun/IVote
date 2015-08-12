5e7eec2c6b1b4fe97c82cb5d6406441b1d6dc1f2
aaronmson <aaronmsun@yahoo.com>
2015-08-12 13:00:24 +0530

    Simulation done! has to create the main method in driver

--- IVote/src/ivote/IVoteService.java
+++ IVote/src/ivote/IVoteService.java
@@ -1,5 +1,122 @@
 package ivote;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class IVoteService {
+
+    boolean isMultipleChoce = false;
+    Question question = new Question();
+    ArrayList<Student> students = new ArrayList<>();
+
+    /**
+     * Set the answers type
+     */
+    public void configureAnsweeType() {
+
+        //create a new question
+        question = new Question();
+
+//radom way to set the question type
+        Random r = new Random();
+        int num = r.nextInt(100);
+        if (num / 2 == 0) {
+            isMultipleChoce = false;
+            System.out.println("Set Question Type as Single Choice!");
+        } else {
+            isMultipleChoce = true;
+            System.out.println("Set Question Type as Multiple Choice!");
+        }
+
+    }
+
+    /**
+     * set the answers as the type
+     */
+    public void configureAnswers() {
+        if (!isMultipleChoce) {
+            Answer r = new Answer();
+            r.answer = "1. Right";
+            Answer w = new Answer();
+            w.answer = "2. Wrong";
+
+            question.answers.add(r);
+            System.out.println("Answer addded to the question:" + r);
+
+            question.answers.add(w);
+            System.out.println("Answer addded to the question:" + w);
+
+        } else {
+            Answer a = new Answer();
+            a.answer = "A";
+            question.answers.add(a);
+            System.out.println("Answer addded to the question:" + a);
+
+            Answer b = new Answer();
+            b.answer = "B";
+            question.answers.add(b);
+            System.out.println("Answer addded to the question:" + b);
+
+            Answer c = new Answer();
+            c.answer = "C";
+            question.answers.add(c);
+            System.out.println("Answer addded to the question:" + c);
+
+            Answer d = new Answer();
+            d.answer = "D";
+            question.answers.add(d);
+            System.out.println("Answer addded to the question:" + d);
+
+        }
+    }
+
+    /**
+     * Submit an answer enter ID and answer
+     */
+    public void submitAnswer() {
+
+        Random r = new Random();
+        int num = r.nextInt(100);
+
+        //if single choce
+        if (!isMultipleChoce) {
+            for (int i = 0; i < num; i++) {
+                Answer a = question.answers.get(r.nextInt(2) % 2);//right or wrong
+                //create unique student name and give the answer
+                Student s = new Student("student" + Integer.toString(i), a);
+                System.out.println("Student :" + s.id + "Answered as :" + s.studentAnswer);
+
+                //increment the count of the answers
+                a.count++;
+                students.add(s);
+
+            }
+        } else {
+            //if miltiple choice
+            for (int i = 0; i < num; i++) {
+                Answer a = question.answers.get(r.nextInt(4) % 4);//pick a random ansewr
+                //create unique student name and give the answer
+                Student s = new Student("student" + Integer.toString(i), a);
+                System.out.println("Student :" + s.id + "Answered as: " + s.studentAnswer);
+                //increment the count of the answers
+                a.count++;
+                students.add(s);
+            }
+        }
+
+    }
+
+    /**
+     * View Statistics of the vote
+     */
+    public void viewStatstics() {
+
+        System.out.println("\n\n***Stastistics***");
+
+        for (Answer answer : question.answers) {
+            System.out.print(answer.answer + " : " + answer.count + ", ");
+        }
+        System.out.println("\n\n");
+    }
 
 }
--- IVote/src/ivote/SimulationDriver.java
+++ IVote/src/ivote/SimulationDriver.java
@@ -1,12 +1,35 @@
 package ivote;
 
+import java.util.Random;
+
 public class SimulationDriver {
 
     /**
-     * @param args the command line arguments
+     * @param args the command line arguments main method
      */
     public static void main(String[] args) {
-       
+        /*
+         * 1) create a question type and configure the answers;  
+         * 2) configure the question for iVote Service; 
+         * 3) randomly generate a number students and the answers; 
+         * 4) submit all the students’ answers to iVote Service; 
+         * 5) call the iVote Service output function to display the result
+         */
+        IVoteService iVoteService = new IVoteService();
+        iVoteService.configureAnsweeType();//multy choce or single choice
+        iVoteService.configureAnswers();//set the answers
+        iVoteService.submitAnswer();//create random number of students and answer to teh question
+
+        iVoteService.viewStatstics();//view statics before the re answer
+
+        //re answer some sutdents
+        Random r = new Random();
+        int num = r.nextInt(iVoteService.students.size());
+        for (int i = 0; i < num; i++) {
+            iVoteService.students.get(i).answer(iVoteService.question.answers.get(r.nextInt(iVoteService.question.answers.size())));
+        }
+
+        iVoteService.viewStatstics();
     }
-    
+
 }
